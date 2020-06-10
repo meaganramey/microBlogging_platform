e96c43156f5261f3d47651a8ea8b09c8e2ac4cfc
Morgan Farrell <farrell131@gmail.com>
2020-06-10 18:30:56 -0400

    actual load spinner

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { LoginFormContainer, MenuContainer, } from "../components";
 import { SignUpFormContainer } from "../components/sign-up-form";
+import Spinner from 'react-bootstrap/Spinner'
 
 export const HomeScreen = () => (
   <>
@@ -8,5 +9,8 @@ export const HomeScreen = () => (
     <h2>Your favorite microblogging platform</h2>
     <LoginFormContainer />
     <SignUpFormContainer />
+    {/* <Spinner animation="border" role="status">
+  <span className="sr-only">Loading...</span>
+</Spinner> */}
   </>
 );
