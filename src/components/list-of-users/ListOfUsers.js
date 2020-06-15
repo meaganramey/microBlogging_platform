575e0ab2c372febf44787123128cf1f575d4ebc7
Meagan Ramey <meagan.ramey@yahoo.com>
2020-06-14 20:07:01 -0400

    Added in lodash library to create a chunked array. This allowed for better visual from the mapped result.

--- src/components/list-of-users/ListOfUsers.js
+++ src/components/list-of-users/ListOfUsers.js
@@ -3,25 +3,80 @@ import { Loader } from "../loader";
 import ProptTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid";
+import array from "lodash/array";
 
 export const DisplayListOfUsers = ({ listOfUsers, loading, error, users }) => {
   useEffect(() => {
     // Update the document title using the browser API
     listOfUsers();
   }, [listOfUsers]);
-let defaultImage = require("../../images/default.png")
-let moment = require('moment')
+  let defaultImage = require("../../images/default.png");
+  let moment = require("moment");
   const memberSince = (createdAt) => {
     // console.log(moment(createdAt).fromNow())
-    return moment(createdAt).fromNow()
-  }
+    return moment(createdAt).fromNow();
+  };
+  let chunkedUsers = array.chunk(users, 4);
   return (
     <>
       <section className="listOfUsers">
         <h1>People you may know:</h1>
         <div className="unorderedListOfUsers">
-          {users.map((user) => {
+          {chunkedUsers.map((row, rowIndex) => {
+            return (
+              <div key={rowIndex} className="row">
+                {row.map((user, colIndex) => {
+                  return (
+                    <div key={colIndex} className="col-sm">
+                      <Card style={{ width: 18 + "rem", textAlign: "center" }}>
+                        <img
+                          className="card-img-top rounded zoom img-fluid"
+                          alt={"User"}
+                          src={
+                            "https://kwitter-api.herokuapp.com/users/" +
+                            user.username +
+                            "/picture"
+                          }
+                          style={{
+                            width: 200 + "px",
+                            minHeight: 20 + "px",
+                            margin: 0 + " auto",
+                          }}
+                          onError={(e) => {
+                            e.target.onerror = null;
+                            e.target.src = defaultImage;
+                          }}
+                        />
+                        <div key={user.username} className="card-body">
+                          <Link
+                            to={"/profiles/" + user.username}
+                            className="btn btn-secondary"
+                          >
+                            {/* {user.displayName} */}
+                            <h2 className="card-title">{user.displayName}</h2>
+                          </Link>
+                          <h6 className="card-text list-group-item">
+                            Username: {user.username}
+                          </h6>
+                          <p className="card-text">
+                            About {user.displayName} :{" "}
+                            {user.about ||
+                              user.displayName +
+                                " is still thinking about this part..."}
+                          </p>
+                          <p className="card-footer">
+                            Member since : {memberSince(user.createdAt)}
+                          </p>
+                        </div>
+                      </Card>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+          {/* {users.map((user) => {
             return (
               <React.Fragment key={uuidv4()}>
                 <Card style={{ width: 18 + "rem", textAlign: "center"}}>
@@ -43,7 +98,7 @@ let moment = require('moment')
                   <div key={user.username} className="card-body">
                     <Link to={"/profiles/" + user.username} className="btn btn-secondary">
                       {/* {user.displayName} */}
-                      <h2 className="card-title">{user.displayName}</h2>
+          {/* <h2 className="card-title">{user.displayName}</h2>
                     </Link>
                     <h6 className="card-text list-group-item">Username: {user.username}</h6>
                     <p className="card-text">
@@ -57,9 +112,8 @@ let moment = require('moment')
                       </p>
                   </div>
                 </Card>
-              </React.Fragment>
-            );
-          })}
+              </React.Fragment> */}
+          ); })} */}
         </div>
       </section>
       {loading && <Loader />}
